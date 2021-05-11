313f388b7af99d426e7c7f29022ce0a317da4cb0
Karan Vaid <vaidkaran89@gmail.com>
2021-05-11 12:53:17 +0530

    added a brand and category picker

--- screens/addVehicle.js
+++ screens/addVehicle.js
@@ -4,24 +4,45 @@ import { StyleSheet, View } from 'react-native';
 import { Colors } from '../styles';
 import { ShadowCard, Picker } from '../components';
 
-const brands = ['maruti suzuki', 'nissan', 'honda', 'ford'];
+const carBrands = ['maruti suzuki', 'nissan', 'honda', 'ford'];
+const bikeBrands = ['tvs', 'baja', 'honda', 'ktm'];
+const categories = ['car', 'bike', 'scooter'];
 
 export default function ({ navigation }) {
   const [selectedVehicleBrand, setSeletedVehicleBrand] = useState(null);
-  const onValueChange = (itemValue, itemIndex) => {
-    if (itemValue !== null) console.log('val changed');
+  const [selectedVehicleCategory, setSelectedVehicleCategory] = useState(null);
+
+  const onCategoryValueChange = (itemValue, itemIndex) => {
+    if (itemValue !== null) console.log('category changed');
+    setSelectedVehicleCategory(itemValue);
+  };
+  const onBrandValueChange = (itemValue, itemIndex) => {
+    if (itemValue !== null) console.log('brand changed');
     setSeletedVehicleBrand(itemValue);
+  };
+
+  const showBrandPicker = () => {
+    if (selectedVehicleCategory !== null) {
+      return (
+        <Picker
+          items={brands}
+          selectedValue={selectedVehicleBrand}
+          onValueChange={onBrandValueChange}
+          defaultLabel="Select a brand"
+        />
+      );
+    }
   };
 
   return (
     <View style={styles.container}>
       <Picker
-        items={brands}
-        selectedValue={selectedVehicleBrand}
-        onValueChange={onValueChange}
-        defaultLabel='Select a brand'
+        items={categories}
+        selectedValue={selectedVehicleCategory}
+        onValueChange={onCategoryValueChange}
+        defaultLabel="Select a Category"
       />
-
+      {showBrandPicker()}
       <View style={styles.cardItemsContainer}></View>
       <StatusBar style="auto" />
     </View>
